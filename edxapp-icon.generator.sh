08f78f2fc27ba6d0e40aa3793a31417ba95506e2
ariestiyansyah <ariestiyansyah.rizky@gmail.com>
2016-08-21 15:54:48 +0700

    first init

---
+++ edxapp-icon.generator.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -e
+
+SOURCE_FILE="$1"
+TARGET_FILE="$2"
+
+run() {
+cat << EOF
+USAGE:
+    $0 SOURCEFILE TARGETDIR
+EOF
+}
+
+
+# Check if ImageMagick installed, if not then brew install ImageMagick
+command -v convert >/dev/null 2>&1 && {
+    echo "Tools are ready...."
+} || {
+    echo "Installing ImageMagick"
+    brew install ImageMagick
+}
+
+# Check parameter
+if [ $# != 2 ];then
+    run
+    exit -1
+fi
+
+# Check Target directory, if not mkdir
+if [ ! -d "$TARGET_FILE" ];then
+    mkdir -p "$TARGET_FILE"
+fi
+
+# Generate icon
+
+info 'Generate Icon-29.png ...'
+convert "$SOURCE_FILE" -resize 29x29 "$TARGET_DIR/Icon-29.png"
+info 'Generate Icon-29@2x.png ...'
+convert "$SOURCE_FILE" -resize 58x58 "$TARGET_FIR/Icon-29@2x.png"
+
+info 'Icon is ready Darth edXius!.'

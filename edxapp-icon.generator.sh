fd2d88eaff43f28ec43b8ad83bbdbc6025218a74
ariestiyansyah <ariestiyansyah.rizky@gmail.com>
2016-08-22 21:37:10 +0700

    fix error on color issue

--- edxapp-icon.generator.sh
+++ edxapp-icon.generator.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+
 set -e
 
 SOURCE_FILE="$1"
 TARGET_DIR="$2"
+
+edXius () {
+     local blue="\033[1;34m"
+     local normal="\033[0m"
+     echo -e "[${blue}edXius${normal}] $1"
+}
 
 run() {
 cat << EOF
@@ -30,22 +37,30 @@ if [ ! -d "$TARGET_DIR" ];then
     mkdir -p "$TARGET_DIR"
 fi
 
-# Generate icon
-info 'Generate Icon-29.png ...'
+edXius 'Generate iTunesArtwork.png...'
+convert "$SOURCE_FILE" -resize 512x512 "$TARGET_DIR/iTunesArtwork.png"
+edXius 'Generate iTunesArtwork@2x.png ...'
+convert "$SOURCE_FILE" -resize 1024x1024 "$TARGET_DIR/iTunesArtwork@2x.png"
+
+edXius 'Generate Icon-29.png ...'
 convert "$SOURCE_FILE" -resize 29x29 "$TARGET_DIR/Icon-29.png"
-info 'Generate Icon-29@2x.png ...'
+edXius 'Generate Icon-29@2x.png ...'
 convert "$SOURCE_FILE" -resize 58x58 "$TARGET_DIR/Icon-29@2x.png"
-info 'Generate Icon-29@3x.png ...'
+edXius 'Generate Icon-29@3x.png ...'
 convert "$SOURCE_FILE" -resize 87x87 "$TARGET_DIR/Icon-29@3x.png"
 
-info 'Generate Icon-40@2x.png ...'
+edXius 'Generate Icon-40.png ...'
+convert "$SOURCE_FILE" -resize 40x40 "$TARGET_DIR/Icon-40.png"
+edXius 'Generate Icon-40@2x.png ...'
 convert "$SOURCE_FILE" -resize 80x80 "$TARGET_DIR/Icon-40@2x.png"
-info 'Generate Icon-40@3x.png ...'
+edXius 'Generate Icon-40@3x.png ...'
 convert "$SOURCE_FILE" -resize 120x120 "$TARGET_DIR/Icon-40@3x.png"
 
-info 'Generate Icon-60@2x.png ...'
+edXius 'Generate Icon-60.png ...'
+convert "$SOURCE_FILE" -resize 60x60 "$TARGET_DIR/Icon-60.png"
+edXius 'Generate Icon-60@2x.png ...'
 convert "$SOURCE_FILE" -resize 120x120 "$TARGET_DIR/Icon-60@2x.png"
-info 'Generate Icon-60@3x.png ...'
+edXius 'Generate Icon-60@3x.png ...'
 convert "$SOURCE_FILE" -resize 180x180 "$TARGET_DIR/Icon-60@3x.png"
 
-info 'Icon is ready Darth edXius!.'
+edXius 'Icon is ready Darth edXius'
